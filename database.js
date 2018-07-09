d1a43240194cf58f85346f2ab9f6a0e538b5b8cf
linhtn95 <linhtrannhat95@gmail.com>
2018-07-09 21:57:10 +0700

    Update database.js

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://linhtn11:hndsl131108442@ds115971.mlab.com:15971/demo-hapi", { useNewUrlParser: true });
+mongoose.connect("connect your mongodb here", { useNewUrlParser: true });
 const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "connection error"));
